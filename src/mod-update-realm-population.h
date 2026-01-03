79fa40dc26fe07e9146cb61cfa8d31d0a01fd8d9
xiaolin338 <517987264@qq.com>
2026-01-03 10:45:54 +0800

    Create mod-update-realm-population.h

---
+++ src/mod-update-realm-population.h
@@ -0,0 +1,21 @@
+#ifndef MOD_UPDATE_REALM_POPULATION_H
+#define MOD_UPDATE_REALM_POPULATION_H
+
+#include "ScriptMgr.h"
+
+class ModUpdateRealmPopulation : public WorldScript
+{
+public:
+    ModUpdateRealmPopulation() : WorldScript("mod_update_realm_population") { }
+
+    void OnAfterConfigLoad(bool reload) override;
+    void OnStartup() override;
+    void OnUpdate(uint32 diff) override;
+
+private:
+    IntervalTimer _populationTimer;
+    uint32 _updateInterval;
+    bool _enabled;
+};
+
+#endif // MOD_UPDATE_REALM_POPULATION_H
